6e0c1b2ad3908bf0b03e76d60a424e009cada6f6
Home-PC\Home <ask4ebuka@gmail.com>
2017-05-02 10:37:24 +0100

    duh

--- rdfCreator.py
+++ rdfCreator.py
@@ -7,8 +7,14 @@ Created on Mon Apr 24 11:13:13 2017
 from rdflib import Graph
 from rdflib import URIRef, BNode, Literal
 from rdflib.namespace import RDF, FOAF
-
+#Graph creation
 g = Graph()
+
+def output_rdf():
+    g.serialize(destination='output.rdf', format='xml')
+
+def add_triple(uri, type, value):
+        g.add(uri, type, value)
 
 #bbc = URIRef("http://www.bbc.co.uk/news/0")
 #linda = BNode() # a GUID is generated
@@ -19,8 +25,3 @@ g = Graph()
 # g.add( (linda, RDF.type, FOAF.Person) )
 # g.add( (linda, RDF.type, Literal('Linda') ) )
 # g.serialize(destination='output.rdf', format='xml')
-def output_rdf():
-    g.serialize(destination='output.rdf', format='xml')
-
-def add_triple(uri, type, value):
-        g.add(uri, type, value)
