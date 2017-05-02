3ec18e628fe5318929fd4ae3817321c96ae86a20
Home-PC\Home <ask4ebuka@gmail.com>
2017-05-02 10:47:59 +0100

    dbpedia change

--- testDBpeadia.py
+++ testDBpeadia.py
@@ -1,10 +1,6 @@
 import spotlight
-import json
 def dbpedia(text):
     annotations = spotlight.annotate('http://model.dbpedia-spotlight.org/en/annotate',
                                       text,
                                      confidence=0.0, support=20)
-    #return (annotations)
-    #print (type(annotations))
-    result = [annotations["URI"]for item in annotations]
-    print (result)
+    return (annotations)
