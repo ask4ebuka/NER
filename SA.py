50d36b6a104a91b50bc0a719c392e241ad164093
Home-PC\Home <ask4ebuka@gmail.com>
2017-05-02 10:42:47 +0100

    sa change

--- SA.py
+++ SA.py
@@ -5,7 +5,6 @@ Created on Mon Apr 24 13:18:30 2017
 @author: Home
 """
 from textblob import TextBlob
-#text = "WASHINGTON -- In the wake of a string of abuses by New York police officers in the 1990s, Loretta E. Lynch, the top federal prosecutor in Brooklyn, spoke forcefully about the pain of a broken trust that African-Americans felt and said the responsibility for repairing generations of miscommunication and mistrust fell to law enforcement."
 def get_sentiment(text):
     blob = TextBlob(text)
     blob.tags           # [('The', 'DT'), ('titular', 'JJ'),
