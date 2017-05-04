4116c16d9f177cd7f0f47dcad9388d75dcb9b73d
Home-PC\Home <ask4ebuka@gmail.com>
2017-05-04 21:25:10 +0100

    latest

--- hello.py
+++ hello.py
@@ -1,18 +1,14 @@
 from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
-@app.route('/success/<name>')
-def success(name):
-   return 'welcome %s' % name
-
-@app.route('/login',methods = ['POST', 'GET'])
+@app.route('/texts',methods = ['POST', 'GET'])
 def login():
    if request.method == 'POST':
       user = request.form['nm']
-      return redirect(url_for('success',name = user))
+      return redirect(url_for('RDF',name = user))
    else:
       user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
+      return redirect(url_for('RDF',name = user))
 
 if __name__ == '__main__':
    app.run(debug = True)
